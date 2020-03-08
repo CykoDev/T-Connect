993e459d3abcab96582a2af5d927887f43aa1c84
Anas Tasadduq <anas.tasadduq@gmail.com>
2020-03-09 01:34:01 +0500

    make the events dynamic

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Event {
+  String id;
   String title;
-  DateTime dateTime;
+  String dateTime;
   String imageUrl;
   String description;
 
   Event({
+    @required this.id,
     @required this.title,
     @required this.dateTime,
     @required this.imageUrl,
--- lib/widgets/event_item.dart
+++ lib/widgets/event_item.dart
@@ -3,10 +3,26 @@ import 'package:flutter/material.dart';
 import '../screens/event_screen.dart';
 
 class EventItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final String date;
+  final String description;
+
+  EventItem(this.id, this.title, this.imageUrl, this.date, this.description);
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Navigator.of(context).pushNamed(EventScreen.routeName),
+      onTap: () => Navigator.of(context).pushNamed(
+        EventScreen.routeName,
+        arguments: {
+          'title': title,
+          'date': date,
+          'imageUrl': imageUrl,
+          'description': description,
+        },
+      ),
       child: Card(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
@@ -18,8 +34,8 @@ class EventItem extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    Text('Title'),
-                    Text('Subtitle'),
+                    Text(title),
+                    Text(description),
                   ],
                 ),
               ),
@@ -30,7 +46,7 @@ class EventItem extends StatelessWidget {
                 child: Container(
                   padding: EdgeInsets.all(10),
                   child: Image.network(
-                    'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+                    imageUrl,
                     fit: BoxFit.contain,
                   ),
                 ),
