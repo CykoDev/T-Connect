1c19c7f599e2a31618d238de8954dee62d9d2379
Anas Tasadduq <anas.tasadduq@gmail.com>
2020-03-08 20:30:29 +0500

    connect app to firebase

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class Event {
   String title;
   DateTime dateTime;
+  String imageUrl;
   String description;
 
   Event({
     @required this.title,
     @required this.dateTime,
+    @required this.imageUrl,
     @required this.description,
   });
 }
