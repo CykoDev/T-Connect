934ddc301dd334a140a7970e4d9833004777055b
Anas Tasadduq <anas.tasadduq@gmail.com>
2020-03-09 22:29:36 +0500

    update models to better work with firebase rtdb

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class User {
+  String id;
   String name;
-  DateTime dateOfBirth;
+  String dateOfBirth;
   String email;
 
   User({
@@ -10,4 +12,19 @@ class User {
     @required this.dateOfBirth,
     @required this.email,
   });
+
+  User.fromSnapshot(DataSnapshot snapshot) {
+    id = snapshot.key;
+    name = snapshot.value['name'];
+    dateOfBirth = snapshot.value['dob'];
+    email = snapshot.value['email'];
+  }
+
+  toJson() {
+    return {
+      "name": name,
+      "dob": dateOfBirth,
+      "email": email,
+    };
+  }
 }
